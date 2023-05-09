2d62bc999ca57142077cbf3d183e87f2cfe7eeb7
tnkfactory <tech@tnkfactory.com>
2023-05-09 10:09:36 +0900

    디자인 커스텀 샘플 모듈 추가

---
+++ custom_1/src/main/java/com/tnkfactory/offerrer/TnkAdManager.kt
@@ -0,0 +1,46 @@
+package com.tnkfactory.offerrer
+
+import android.content.Context
+import com.tnkfactory.ad.TnkAdConfig
+import com.tnkfactory.ad.TnkOfferwall
+import com.tnkfactory.offerrer.scene.TnkEmbedActivity
+
+/**
+ * @author hanago
+ * @email hans@tnkfactory.com
+ * @since 2023/04/24
+ **/
+object TnkAdManager {
+
+    fun setUserInfo(context: Context, coppa: Boolean, userName: String) {
+        TnkOfferwall(context).run {
+            setCOPPA(coppa)
+            setUserName(userName)
+        }
+    }
+
+    fun startEmbedActivity(context: Context) {
+        TnkAdConfig.layoutConfig.setListHeader(0)
+        TnkEmbedActivity.start(context)
+    }
+
+    fun startDefaultActivity(context: Context) {
+        TnkAdConfig.layoutConfig.setListHeader(1)
+        TnkOfferwall(context).startOfferwallActivity(context)
+    }
+
+
+    fun useTerms(flag: Boolean) {
+        TnkAdConfig.useTermsPopup = flag
+    }
+
+    fun usePointUnit(flag: Boolean) {
+        TnkAdConfig.usePointUnit = flag
+    }
+
+    fun useCuration(flag: Boolean) {
+        TnkAdConfig.useCuration = flag
+    }
+
+    fun setCustomClass() {}
+}
